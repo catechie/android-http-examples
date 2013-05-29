bbfcbc6a689fd03aa8d36b67f2276aee91a8c429
Jesse Wilson <jwilson@squareup.com>
2013-05-29 11:48:36 -0400

    tidy the code

--- app/src/com/publicobject/pictures/PicturesActivity.java
+++ app/src/com/publicobject/pictures/PicturesActivity.java
@@ -3,13 +3,9 @@ package com.publicobject.pictures;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class PicturesActivity extends Activity
-{
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class PicturesActivity extends Activity {
+  @Override public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+  }
 }
